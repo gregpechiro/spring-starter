c872a811bade031c0a8ee1b24d0fb4805bbe9fbd
scottcagno <scottiecagno@gmail.com>
2014-07-15 23:09:55 -0400

    initial commit

--- src/main/groovy/com/cagnosolutions/starter/app/Application.groovy
+++ src/main/groovy/com/cagnosolutions/starter/app/Application.groovy
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
 /**
  * Created by Scott Cagno.
  * Copyright Cagno Solutions. All rights reserved.
