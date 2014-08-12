1160f9b917208c1d722179738f4aeda5e080b75f
Scott Cagno <scottiecagno@gmail.com>
2014-08-12 14:33:16 -0400

    Update Application.groovy

--- src/main/groovy/com/cagnosolutions/starter/app/Application.groovy
+++ src/main/groovy/com/cagnosolutions/starter/app/Application.groovy
@@ -1,5 +1,6 @@
 package com.cagnosolutions.starter.app
 
+import groovy.transform.CompileStatic
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
