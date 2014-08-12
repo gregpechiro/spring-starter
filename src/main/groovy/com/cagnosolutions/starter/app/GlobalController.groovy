89f8d75c42b46cc84c78f14351532e354c021093
Scott Cagno <scottiecagno@gmail.com>
2014-08-12 14:36:49 -0400

    Update GlobalController.groovy

--- src/main/groovy/com/cagnosolutions/starter/app/GlobalController.groovy
+++ src/main/groovy/com/cagnosolutions/starter/app/GlobalController.groovy
@@ -1,5 +1,6 @@
 package com.cagnosolutions.starter.app
 
+import groovy.transform.CompileStatic
 import com.cagnosolutions.starter.app.user.UserData
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
@@ -17,49 +18,50 @@ import java.security.Principal
  */
 
 @CompileStatic
-@Controller
+@Controller(value = "indexController")
 class IndexController {
-    @RequestMapping(value=["/"], method=[RequestMethod.GET])
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     String index() {
         "index"
     }
 }
 
 @CompileStatic
-@Controller
+@Controller(value = "authController")
 class Authentication {
 
     @Autowired
     UserData userData
 
-    @RequestMapping(value=["/login"])
+    @RequestMapping(value = "/login")
     String login() {
         "login"
     }
 
-    @RequestMapping(value=["/secure/login"], method=[RequestMethod.GET])
+    @RequestMapping(value = "/secure/login", method = RequestMethod.GET)
     String secureLogin(@RequestParam String forward, HttpSession session, Principal principal) {
-        if(principal.name != "admin"){
-            def user = userData.findOne(principal.name)
+        if(principal.name != "admin") {
+            def user = userData.findOne principal.name
             user.lastSeen = System.currentTimeMillis()
             userData.save user
         }
         session.setAttribute "authenticated", principal.name
-        "redirect:/secure/" + forward
+        "redirect:/secure/$forward"
     }
 }
 
 @CompileStatic
-@Controller
+@Controller(value = "errorController")
 class ErrorHandler {
 
-    @ExceptionHandler(value=[Exception.class, RuntimeException.class])
+    @ExceptionHandler(value = [Exception.class, RuntimeException.class])
     String errors(Exception e, Model model) {
         def stack = []
         for (frame in e.getStackTrace()) {
             stack << frame.toString()
         }
         model.addAllAttributes([message: e.getLocalizedMessage(), exception: stack.join('\n')])
-        return "error";
+        "error"
     }
 }
