d51c5444471976a47b386872d7ecc1f0dbddb03c
gregpechiro <gregpechiro@yahoo.com>
2014-09-08 12:09:32 -0400

    added emailer

--- src/main/groovy/com/cagnosolutions/starter/app/GlobalController.groovy
+++ src/main/groovy/com/cagnosolutions/starter/app/GlobalController.groovy
@@ -1,23 +1,16 @@
 package com.cagnosolutions.starter.app
-
-import groovy.transform.CompileStatic
 import com.cagnosolutions.starter.app.user.UserService
+import groovy.transform.CompileStatic
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.PropertySource
-import org.springframework.core.env.Environment
-import org.springframework.mail.javamail.JavaMailSender
-import org.springframework.mail.javamail.JavaMailSenderImpl
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
+
 import javax.servlet.http.HttpSession
 import java.security.Principal
-
 /**
  * Created by Scott Cagno.
  * Copyright Cagno Solutions. All rights reserved.
@@ -70,32 +63,4 @@ class ErrorHandler {
         model.addAllAttributes([message: e.getLocalizedMessage(), exception: stack.join('\n')])
         "error"
     }
-}
-
-@Configuration
-@PropertySource("classpath:application.yml")
-public class MailConfig {
-
-	@Autowired
-	private Environment env;
-
-	@Bean(name = "mailSender")
-	public JavaMailSender javaMailSender() {
-		JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-		javaMailSender.setHost(env.getProperty("mail.host"));
-		javaMailSender.setPort(Integer.parseInt(env.getProperty("mail.port")));
-		javaMailSender.setUsername(env.getProperty("mail.user"));
-		javaMailSender.setPassword(env.getProperty("mail.pass"));
-		javaMailSender.setJavaMailProperties(getMailProperties());
-		return javaMailSender;
-	}
-
-	private Properties getMailProperties() {
-		Properties properties = new Properties();
-		properties.setProperty("mail.transport.protocol", "smtp");
-		properties.setProperty("mail.smtp.auth", "true");
-		properties.setProperty("mail.smtp.starttls.enable", "true");
-		properties.setProperty("mail.debug", "true");
-		return properties;
-	}
 }
