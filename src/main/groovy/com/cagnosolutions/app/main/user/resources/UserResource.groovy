6c71d7888fcbec4a93bbd31d34ea36112c32160f
scottcagno <scottiecagno@gmail.com>
2014-11-29 19:37:07 -0500

    started implementing hateos resource support

---
+++ src/main/groovy/com/cagnosolutions/app/main/user/resources/UserResource.groovy
@@ -0,0 +1,10 @@
+package com.cagnosolutions.app.main.user.resources
+
+import com.cagnosolutions.app.main.user.User
+import groovy.transform.CompileStatic
+import org.springframework.hateoas.ResourceSupport
+
+@CompileStatic
+class UserResource extends ResourceSupport {
+	User user
+}
