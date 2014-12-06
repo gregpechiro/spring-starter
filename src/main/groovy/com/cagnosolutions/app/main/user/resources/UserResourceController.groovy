915c6cf76006ad819bc8c554da8f0df1c43c487f
scottcagno <scottiecagno@gmail.com>
2014-12-05 23:59:38 -0500

    working with rest stuff...

--- src/main/groovy/com/cagnosolutions/app/main/user/resources/UserResourceController.groovy
+++ src/main/groovy/com/cagnosolutions/app/main/user/resources/UserResourceController.groovy
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody
 
 @CompileStatic
 @Controller
-@RequestMapping(value = "/api/users")
+@RequestMapping(value = "/resource/users")
 class UserResourceController {
 
 	@Autowired
